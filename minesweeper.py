f19bb7312490ff288c66cc8a1546c14654c6de1d
irmnpreet <41720146+irmnpreet@users.noreply.github.com>
2019-09-23 22:25:49 +1000

    Update minesweeper.py
    final update

--- minesweeper.py
+++ minesweeper.py
@@ -137,7 +137,97 @@ def prepareWindow():
             b.grid(row=x+1, column=y, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
             buttons[x].append(b)
 
+def restartGame():
+    global gameover
+    gameover = False
+    #destroy all - prevent memory leak
+    for x in window.winfo_children():
+        if type(x) != tkinter.Menu:
+            x.destroy()
+    prepareWindow()
+    prepareGame()
 
+def clickOn(x,y):
+    global field, buttons, colors, gameover, rows, cols
+    if gameover:
+        return
+    buttons[x][y]["text"] = str(field[x][y])
+    if field[x][y] == -1:
+        buttons[x][y]["text"] = "*"
+        buttons[x][y].config(background='red', disabledforeground='black')
+        gameover = True
+        tkinter.messagebox.showinfo("Game Over", "You have lost.")
+        #now show all other mines
+        for _x in range(0, rows):
+            for _y in range(cols):
+                if field[_x][_y] == -1:
+                    buttons[_x][_y]["text"] = "*"
+    else:
+        buttons[x][y].config(disabledforeground=colors[field[x][y]])
+    if field[x][y] == 0:
+        buttons[x][y]["text"] = " "
+        #now repeat for all buttons nearby which are 0... kek
+        autoClickOn(x,y)
+    buttons[x][y]['state'] = 'disabled'
+    buttons[x][y].config(relief=tkinter.SUNKEN)
+    checkWin()
+
+def autoClickOn(x,y):
+    global field, buttons, colors, rows, cols
+    if buttons[x][y]["state"] == "disabled":
+        return
+    if field[x][y] != 0:
+        buttons[x][y]["text"] = str(field[x][y])
+    else:
+        buttons[x][y]["text"] = " "
+    buttons[x][y].config(disabledforeground=colors[field[x][y]])
+    buttons[x][y].config(relief=tkinter.SUNKEN)
+    buttons[x][y]['state'] = 'disabled'
+    if field[x][y] == 0:
+        if x != 0 and y != 0:
+            autoClickOn(x-1,y-1)
+        if x != 0:
+            autoClickOn(x-1,y)
+        if x != 0 and y != cols-1:
+            autoClickOn(x-1,y+1)
+        if y != 0:
+            autoClickOn(x,y-1)
+        if y != cols-1:
+            autoClickOn(x,y+1)
+        if x != rows-1 and y != 0:
+            autoClickOn(x+1,y-1)
+        if x != rows-1:
+            autoClickOn(x+1,y)
+        if x != rows-1 and y != cols-1:
+            autoClickOn(x+1,y+1)
+
+def onRightClick(x,y):
+    global buttons
+    if gameover:
+        return
+    if buttons[x][y]["text"] == "?":
+        buttons[x][y]["text"] = " "
+        buttons[x][y]["state"] = "normal"
+    elif buttons[x][y]["text"] == " " and buttons[x][y]["state"] == "normal":
+        buttons[x][y]["text"] = "?"
+        buttons[x][y]["state"] = "disabled"
+
+def checkWin():
+    global buttons, field, rows, cols
+    win = True
+    for x in range(0, rows):
+        for y in range(0, cols):
+            if field[x][y] != -1 and buttons[x][y]["state"] == "normal":
+                win = False
+    if win:
+        tkinter.messagebox.showinfo("Gave Over", "You have won.")
+
+    if os.path.exists("config.ini"):
+        loadConfig()
+    else:
+        saveConfig()
+
+createMenu()
 
 prepareWindow()
 prepareGame()
